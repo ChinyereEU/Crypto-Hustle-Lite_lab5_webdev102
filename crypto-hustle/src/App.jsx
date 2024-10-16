eecd1bfc430b795ab99dca789c8dc514f52a6cd0
Chinyere Ugwuanyi <ceu15@scarletmail.rutgers.edu>
2024-10-15 20:51:14 -0400

    used useEffect() hook to call API. Implemented await/async to make API calls so the webpage doesn't load data before retrieving the content from the API call. Inside useEffect() hook, defined async function (fetchAllCoinData()) that uses 'fetch' to make API call. Saved JSON response returned from API call to list state variable. Made sure fetchAllCoinData() works by calling it and handling potential errors that could pop up.

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -6,7 +6,25 @@ import './App.css'
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  /**'list' useState() variable */
+  const [list, setList] = useState(null)
+
+  /**useEffect() hook */
+  useEffect(() => {
+
+    /**async func that uses fetch to make call to API */
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY
+      );
+      /**save JSON response returned to the list state variable */
+      const json = await response.json();
+      setList(json);
+    }
+
+    /**call fetchAllCoinData() & handle any errors that could pop up with it */
+    fetchAllCoinData().catch(console.error);
+  }, []);
 
   return (
     
