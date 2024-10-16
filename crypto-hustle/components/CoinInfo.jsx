d4129fac417010cc59df8738dbe3a6301078da91
Chinyere Ugwuanyi <ceu15@scarletmail.rutgers.edu>
2024-10-15 21:44:21 -0400

    added a useEffect() hook passing in symbol in closing [] so that hook runs only when symbol changes.

--- crypto-hustle/components/CoinInfo.jsx
+++ crypto-hustle/components/CoinInfo.jsx
@@ -8,4 +8,11 @@ const CoinInfo = ({image, name, symbol}) => {
 
     //state variable for price set to null
     const [price, setPrice] = useState(null)
+
+    //useEffect() hook passing in 'symbol' in closing []
+    //so that instead of useEffect running on every render, it runs when the symbol we pass in changes
+    //everytime we give a new coin symbol to get the info for, useEffect() hook will run again
+    useEffect(() => {
+
+    }, [symbol]);
 }
