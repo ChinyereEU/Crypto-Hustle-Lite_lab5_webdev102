3c7588da38d21b17f2fbc628f191b2b1bd05d074
Chinyere Ugwuanyi <ceu15@scarletmail.rutgers.edu>
2024-10-15 22:00:21 -0400

    implemented a way to get price data for one coin at a time, then defined async function to call API for this data.

--- crypto-hustle/components/CoinInfo.jsx
+++ crypto-hustle/components/CoinInfo.jsx
@@ -13,6 +13,23 @@ const CoinInfo = ({image, name, symbol}) => {
     //so that instead of useEffect running on every render, it runs when the symbol we pass in changes
     //everytime we give a new coin symbol to get the info for, useEffect() hook will run again
     useEffect(() => {
-
+        //function to get price data for one coin at a time
+        const getCoinPrice = async() => {
+            const response = await fetch(
+                `https://min-api.cryptocompare.com/data/price?fsyms=${symbol}&tsyms=USD&api_key` + API_KEY
+            );
+            const json = await response.json();
+            setPrice(json);
+        }
+        getCoinPrice().catch(console.error);
     }, [symbol]);
+
+    return(
+        <div>
+            {price ? (
+
+            ) : null
+            }
+        </div>
+    );
 }
